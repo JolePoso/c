6b4ebf583d6cfaaf3caf39b04d43d25e3817301e
JolePoso <96224887+JolePoso@users.noreply.github.com>
2022-01-13 07:50:35 +0100

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+
+    int main() (
+        float F;
+
+        printf("Unesi temperaturu u fahrenheitima\n");
+        scanf("%d", &F)
+
+        float C=(F-32)*9/5;
+        float K=C+273.15;
+
+        printf("%d\.2f\", C);
+        printf("%d\.2f\n", K);
+
+                return 0;
+        }
