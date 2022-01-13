9c6c63e867502ee66e85ecabfe3561915605f3a4
JolePoso <96224887+JolePoso@users.noreply.github.com>
2022-01-13 08:11:45 +0100

    Add files via upload

---
+++ Untitled4.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+
+int main () {
+    float staracijena;
+    float novacijena;
+    printf("unesi staru i novu cijenu proizvoda\n");
+    scanf("%f %f", &staracijena, &novacijena);
+
+    int postotaksnizenja=((float)(staracijena-novacijena)/staracijena)*100;
+    printf("%d\n", postotaksnizenja);
+    return 0;
+    }
+
+
+
