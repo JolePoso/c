2d01ffb29e0a42df900dc5e98c0bd563d0e6241f
JolePoso <96224887+JolePoso@users.noreply.github.com>
2022-01-13 08:40:50 +0100

    Add files via upload

---
+++ Untitled5.c
@@ -0,0 +1,20 @@
+    #include <stdio.h>
+
+    int main () {
+    int d;
+    printf("unesi promjer vlaka");
+    scanf("%d", d);
+
+    float opseg=d*3.1315;
+
+    printf("Napravi &0.f okretaja", 40000/opseg);
+
+
+
+
+
+
+
+
+
+    }
