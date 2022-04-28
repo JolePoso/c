7d125df07986cfa8fe1a1ba0875a1dfbb8e19f29
JolePoso <96224887+JolePoso@users.noreply.github.com>
2022-04-28 09:14:17 +0200

    Add files via upload

---
+++ trokut.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+  
+  
+   printf ("Unesi a\n");
+    scanf("%d", &a);
+   
+   printf ("Unesi b\n");
+    scanf("%d", &b);
+   
+   
+   
+   
+     		|\
+			| \
+			|  \
+			|   \
+			|    \
+			|     \
+			|      \ 
+			|       \
+			|        \
+			|         \
+			|          \
+			|___________\
+}
+   
+   
+   
+   
+    
+
+
